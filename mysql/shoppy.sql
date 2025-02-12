5ad05f79693e718871012be8819f1c84f6e74582
Yuna-Kim98 <dbsk4030@gmail.com>
2025-02-12 17:29:26 +0900

    25. 02. 12 commit

--- mysql/shoppy.sql
+++ mysql/shoppy.sql
@@ -1,107 +1,105 @@
 /**
-	Shoppy 테이블 정의
-**/
--- shoppy로 시작하는 모든 테이블 조회
-select * from information_schema.tables where table_name like 'shoppy%';
+	SHOPPY 테이블 정의
+*/
+show databases;
 -- shoppy_member 테이블 생성
 use hrdb2019;
 select database();
 show tables;
+-- shoppy로 시작하는 모든 테이블 조회
+select * from information_schema.tables
+	where table_name like 'shoppy%';
+    
 create table shoppy_member(
 	id				varchar(30)		primary key,
-    pwd				varchar(50)		not null,
+    pwd 			varchar(50)		not null,
     name			varchar(10)		not null,
-    phone			char(13)		not null,
-    emailname		varchar(20)		not null,
-    emaildomain		varchar(20)		not null,
-    zipcode			char(5),
-    address			varchar(80),
-    mdate			datetime
+    phone 			char(13)		not null,
+    emailname 		varchar(20)		not null,
+    emaildomain 	varchar(20)		not null,
+    zipcode 		char(5),	
+    address 		varchar(80),
+    mdate 			datetime		
 );
+
 desc shoppy_member;
+select * from shoppy_member;
+delete from shoppy_member where id = 'asdfd';
+
+-- 'test' 중복체크 : 결과를 count 함수로 반환 - 0: 사용가능, 1: 사용불가능
+select count(id) as result from shoppy_member where id = 'test9';
 
 select * from shoppy_member;
 
--- 'test1' 아이디 중복체크 : count 함수로 결과 반환
-select count(id) as result from shoppy_member where id = 'test1';
-
--- 테스트용 임시 데이터 
-insert into shoppy_member(id, pwd, name, phone, emailname, emaildomain, zipcode, address, mdate)
-values('test1234', '1234', '김철수', '010-1234-5678', 'hong1234', 'gmail.com', null, null, now());
-
---  login
-select count(*) as result_rows from shoppy_member where id = 'test1' and pwd = '2222';
-
--- shoppy_product
+-- login
+select count(*) as result_rows from shoppy_member
+	where id = 'test11' and pwd = '1111'; 
+    
+    
+    
+    
+    
+    
 use hrdb2019;
-select * from information_schema.tables where table_name like 'shoppy%';
-drop table shoppy_product;
+select * from information_schema.tables
+	where table_name like 'shoppy%';
+    
+-- shoppy_product
 create table shoppy_product(
-	pid		int				primary key			auto_increment,
-    pname	varchar(50)		not null,
-    price	int,
+	pid 	int			primary key		auto_increment,
+    pname 	varchar(50)		not null,
+    price 	int,
     description		varchar(200),
     upload_file		json,
-    source_file		json, -- 다른 db 프로그램에서는 json 타입으로 설정되지 않을 수 있음
+    source_file		json,
     pdate			datetime
 );
-
-show tables;
 desc shoppy_product;
-select * from shoppy_product;
-desc shoppy_product;
-select pid,
-		pname as name,
-        price,
-        description as info,
-        upload_file as image
-from shoppy_product;
-
-select pid,
-            pname as name,
-            price,
-            description as info,
-            concat('http://localhost:9000/', upload_file) as image,
-            source_file,
-            pdate
-        from shoppy_product;
-        
-set sql_safe_updates = 0; -- 해제 : 0, 설정: 1
+set sql_safe_updates = 0;
 delete from shoppy_product;
 commit;
 select * from shoppy_product;
-select upload_file from shoppy_product;
-
-select pid,
-            pname as name,
-            price,
-            description as info,
-            concat('http://localhost:9000/', upload_file->> '$[0]') as image, -- 번지수를 지정. 데이터 타입이 json일 때만 사용 가능.
+drop table shoppy_product;
+select 
+            pid, pname as name,
+            price, description as info,
+            concat('http://localhost:9000/', upload_file->>'$[0]') as image, 
             source_file,
             pdate
         from shoppy_product;
-desc shoppy_product;
-select * from shoppy_product where pid = 3; -- 컬럼명을 명확하게 주는 것이 유지보수 관점에서 효율적임
-select pid,
+
+select 
+		pid,
 		pname,
         price, 
         description,
         upload_file as uploadFile,
         source_file as sourceFile,
         pdate,
-        concat('http://localhost:9000/', upload_file->> '$[0]') as image,
+        concat("http://localhost:9000/", upload_file ->> '$[0]') as image,
         json_array(
-			concat('http://localhost:9000/', upload_file->> '$[0]'),
-            concat('http://localhost:9000/', upload_file->> '$[1]'),
-            concat('http://localhost:9000/', upload_file->> '$[2]')
+			concat("http://localhost:9000/", upload_file ->> '$[0]'),
+            concat("http://localhost:9000/", upload_file ->> '$[1]'),
+            concat("http://localhost:9000/", upload_file ->> '$[2]')
         ) as imgList,
         json_arrayagg(
 			concat('http://localhost:9000/', jt.filename)
         ) as detailImgList
-from shoppy_product, 
-	json_table(shoppy_product.upload_file, '$[*]' columns( filename varchar(100) path '$')) as jt -- 배열 생성 함수
-where pid = 4
-group by pid;
+	from shoppy_product, 
+		json_table(shoppy_product.upload_file, '$[*]'
+			columns( filename varchar(100) path '$')) as jt 
+        where pid = 3
+        group by pid;
+    
+select upload_file from shoppy_product;
 
-show tables;
-select * from shoppy_member;
+select * from shoppy_product;
+
+-- pid, pname, price, description, upload_file 0번지 이미지 
+select pid,
+		pname,
+        price,
+        description,
+        concat("http://localhost:9000/", upload_file ->> '$[0]') as image
+from shoppy_product
+where pid in (3, 5, 6);
